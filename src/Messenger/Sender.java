172e99ed71414068c27bea5e2327187752ff9ea7
ahsanul1404 <99856469+ahsanul1404@users.noreply.github.com>
2022-03-28 10:55:52 +0700

    Update Sender.java

--- src/Messenger/Sender.java
+++ src/Messenger/Sender.java
@@ -6,27 +6,27 @@ package Messenger;
     public class Sender {
     public static void main(String[] args) {
         StdMessenger aidaStd = new StdMessenger(); 
-        MailMessenger aidaMail = new MailMessenger();
+        MailMessenger ahsaMail = new MailMessenger();
         
 //        StdMessenger Test
         System.out.println("= Test_Std_Messenger_Started =");
         System.out.println("Sending a Messege");
-        aidaStd.sendMessege("Hai guys", "Would you have a breakfast","In the Restaurant");
+        ahsaStd.sendMessege("Hai guys", "Would you have a breakfast","In the Restaurant");
         System.out.println("= Test_Std_Messenger_Finished =");
         System.out.println(""); System.out.println("");
 //        Mail Messenger Test
         System.out.println("= Test_Mail_Messenger_Started =");
         System.out.println("Sending a Messege"); // Not Connected Yet.
-        aidaMail.sendMessege("Whatsup", "How are you", "Lets play together?");
+        ahsaMail.sendMessege("Whatsup", "How are you", "Lets play together?");
         
-        aidaMail.disconnect(); //Disconnecting
+        ahsaMail.disconnect(); //Disconnecting
         
         System.out.println("Connecting"); // Connecting
-        aidaMail.connect(); //Connected
+        ahsaMail.connect(); //Connected
         
 //        Resend
         System.out.println("Resend a Messege");
-        aidaMail.sendMessege("Whatsup", "How are you", "Lets play together?");
+        ahsaMail.sendMessege("Whatsup", "How are you", "Lets play together?");
         System.out.println("= Test_Mail_Messenger_Finished =");
         }
     }
